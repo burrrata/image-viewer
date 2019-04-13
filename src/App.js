8cd625d7a50954f8cc39227ee791e8db9f741973
anon <42473928+burrrata@users.noreply.github.com>
2019-04-13 00:03:30 +0000

    _

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// testing the random rotation
+const APODURLs = [
+'https://apod.nasa.gov/apod/image/1904/SaturnMoon_Schmitz_960.jpg',
+'https://apod.nasa.gov/apod/image/1904/AzurePlumesNorway_Sutie_960.jpg',
+'https://apod.nasa.gov/apod/image/1904/scorpio_guisard_960.jpg',
+'https://apod.nasa.gov/apod/image/1904/ISS4panelMar28Addis1024.jpg',
+'https://apod.nasa.gov/apod/image/1904/STScI-H-1912b-panstarrs1024.jpg',
+'https://apod.nasa.gov/apod/image/1904/potw1913aM2_1024.jpg',
+'https://apod.nasa.gov/apod/image/1904/HorseheadFlame_Zauner_960.jpg',
+'https://apod.nasa.gov/apod/image/1904/IssMoon_Holland_960.jpg']
+
+
 // The main React component
 class App extends Component {
 
@@ -8,15 +20,19 @@ class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			images: [`https://apod.nasa.gov/apod/image/1904/JMD_Rosette_Rotated.jpg`]
+			URLs: APODURLs,
 		}
 	}
 
+	// returns a random URL from the state
+	getRandomURL() {
+		return this.state.URLs[Math.floor(Math.random()*this.state.URLs.length)]
+	}
 	// render the images
   render() {
     return (
       <div className='app'>
-				<img src={this.state.images[0]} />
+				<img src={this.getRandomURL()} />
     	</div>
     );
   }
