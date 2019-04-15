087dbd2a4953afa2c69bf5117a4cba19d78ce89e
anon <42473928+burrrata@users.noreply.github.com>
2019-04-15 20:05:52 +0000

    rewrote using hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, Component} from 'react'
+import React, {useCallback, useState, useEffect, Component} from 'react'
 import Dropzone, {useDropzone} from 'react-dropzone'
 import './App.css'
 
@@ -14,88 +14,77 @@ const APODURLs = [
 'https://apod.nasa.gov/apod/image/1904/IssMoon_Holland_960.jpg']
 
 
-// TODO have this push the binaryStr to the App component state
-// File drop zone
-function FileDropzone() {
-
-	// process files when they are dropped
-  const onDrop = useCallback(acceptedFiles => {
-    const reader = new FileReader()
-    reader.onabort = () => console.log('file reading was aborted')
-    reader.onerror = () => console.log('file reading has failed')
-    reader.onload = () => {
-      // Do whatever you want with the file contents
-      const binaryStr = reader.result
-      console.log(binaryStr)
-    }
-    acceptedFiles.forEach(file => reader.readAsBinaryString(file))
-  }, [])
-
-	// not sure what this does
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()} className='over'>
-			<input {...getInputProps()} onDrop={() => this.props.onDrop()} />
-			<p>
-				Click to upload a text file.
-			</p>	
-    </div>
-  )
-}
-
-
-// TODO have this pull the state from the App component
-// Image display component
-function Image(props) {
-	return(
-		<img src={props.value} className='under' />
-	)
-}
-
-
-// The main React component
-// - this manages the state for the App
-class App extends Component {
-
-	// init the state
-	constructor(props) {
-		super(props)
-		this.state = {
-			URLs: null,
-			currentURL: 'https://apod.nasa.gov/apod/image/1904/M87bh_EHT_2629.jpg',
-		}
-	}
-
-	// regularly updates the state currentURL with a random item from URLs
-	componentDidMount() {
-		this.interval = setInterval(() => {
-			if (this.state.URLs != null) {
-				this.setState({
-					currentURL: this.state.URLs[Math.floor(Math.random()*this.state.URLs.length)]
-				})
-			} else {
-				this.setState({
-					currentURL: this.state.currentURL
-				})
-			}
-		}, 5000)	
-	}
-	componentWillUnmount() {
-		clearInterval(this.interval)
-	}
+// This manages the state for the App
+function App() {
 	
-	// FILE UPLOAD STUFF GOES HERE
+	// init the state
+	const [URLs, setURLs] = useState(null)
+	const [currentURL, setCurrentURL] = useState('https://apod.nasa.gov/apod/image/1904/M87bh_EHT_2629.jpg')
+	const [dropzoneView, setDropzoneView] = useState('over')
+	
+	// parse the uploaded files and use setURLs to add them to the state
+	function FileDropzone() {
+		// process files when they are dropped
+		const onDrop = useCallback(acceptedFiles => {
+			const reader = new FileReader()
+			reader.onabort = () => console.log('file reading was aborted')
+			reader.onerror = () => console.log('file reading has failed')
+			reader.onload = () => {
+				// binaryStr is a string with each URL separated by a space
+				// we then split the string by spaces and add each new string to
+				// an array where each URL is an item
+				const binaryStr = reader.result
+				const droppedURLArray = binaryStr.split(/(\s)/).filter( e => e.trim().length > 0)
+				// update the state
+				setURLs(droppedURLArray)
+			}
+			acceptedFiles.forEach(file => reader.readAsBinaryString(file))
+		}, [])
+		// not sure what this does
+		const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+		// return the dropzone component
+		return (
+			<div {...getRootProps()} className={dropzoneView}>
+				<input {...getInputProps()} />
+				<p>
+					Click to upload a text file.
+				</p>	
+			</div>
+		)
+	}
 
-	// render the images
-  render() {
-    return (
-      <div className='app'>
-				<FileDropzone />
-				<Image value={this.state.currentURL} />
-    	</div>
-    )
-  }
+	// Image display component
+	function Image() {
+		return(
+			<img src={currentURL} className='under' alt='' />
+		)
+	}
+
+	// randomly select the currentURL from the URLs list on a recurring basis
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (URLs != null) {
+				setDropzoneView('hidden')
+				const newURL = URLs[Math.floor(Math.random()*URLs.length)]
+				setCurrentURL(newURL)
+				// Check the state for testing
+				//console.log(URLs)
+				//console.log(`URLs length: ${URLs.length}`)
+				//console.log(currentURL)
+			}
+		}, 3000)
+		return function cleanup() {
+			clearInterval(interval)	
+		}
+	})
+
+	// return the app components
+	return (
+		<div className='app'>
+			<FileDropzone />
+			<Image />
+		</div>
+	)
 }
 
 // export the app
