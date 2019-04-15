c587ddd465d6314881b3d8d393edb7347d83107d
anon <42473928+burrrata@users.noreply.github.com>
2019-04-15 20:49:21 +0000

    Updates

---
+++ precache-manifest.175551c869d4b1201a0ca6dc142de473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f62e7415f389cec1a0",
+    "url": "/image-viewer/static/js/runtime~main.417971dc.js"
+  },
+  {
+    "revision": "b43a179dc14982674abd",
+    "url": "/image-viewer/static/js/main.04cce726.chunk.js"
+  },
+  {
+    "revision": "3f54ec581944084e806c",
+    "url": "/image-viewer/static/js/2.4f55aaa9.chunk.js"
+  },
+  {
+    "revision": "b43a179dc14982674abd",
+    "url": "/image-viewer/static/css/main.2ff6f4c5.chunk.css"
+  },
+  {
+    "revision": "9ecab27ea84028b4a8696bd70bb8d80e",
+    "url": "/image-viewer/index.html"
+  }
+];
